a206420622577da05e2b1ad4c6b206fb5c0b287a
em-g73 <emerymgrace@gmail.com>
2026-01-03 00:08:20 -0500

    Added part 8 exercises 6-8

---
+++ part08/part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -0,0 +1,60 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Program {
+
+    public static void main(String[] args) {
+        HashMap<String, String> hashmap = new HashMap<>();
+        hashmap.put("f.e", "for example");
+        hashmap.put("etc.", "and so on");
+        hashmap.put("i.e", "more precisely");
+
+        printKeys(hashmap);
+        System.out.println("---");
+        printKeysWhere(hashmap, "i");
+        System.out.println("---");
+        printValuesOfKeysWhere(hashmap, ".e");
+    }
+    
+    public static void printKeys(HashMap<String,String> hashmap) {
+        ArrayList<String> keys = new ArrayList<>();
+        
+        for (String key: hashmap.keySet()) {
+            keys.add(key);
+        }
+        
+        for (String key: keys) {
+            System.out.println(key);
+        }
+    }
+    
+    public static void printKeysWhere(HashMap<String,String> hashmap, String text) {
+        ArrayList<String> keys = new ArrayList<>();
+        
+        for (String key: hashmap.keySet()) {
+            keys.add(key);
+        }
+        
+        for (String key: keys) {
+            if (key.contains(text)) {
+                System.out.println(key);
+            }
+        }
+    }
+    
+    public static void printValuesOfKeysWhere(HashMap<String,String> hashmap, String text) {
+        ArrayList<String> keys = new ArrayList<>();
+        
+        for (String key: hashmap.keySet()) {
+            keys.add(key);
+        }
+        
+        for (String key: keys) {
+            if (key.contains(text)) {
+                System.out.println(hashmap.get(key));
+            }
+        }
+    }
+    
+}
